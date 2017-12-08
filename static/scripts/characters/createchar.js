2e9ed6267645e7147fcb85c7f63ae688f225be95
mateogolf <rodriguezml11@mail.wlu.edu>
2017-12-08 14:49:34 -0500

    Avatar layers saved and rendered in window

--- static/scripts/characters/createchar.js
+++ static/scripts/characters/createchar.js
@@ -67,83 +67,107 @@ function loadCharSheetCanvas() {
         if(tileX<2 && tileY<4){
             console.log("Base layer")
             avatar.base = allSprites[tileX][tileY];
-            tilectx = base.getContext("2d");
-            tilectx.clearRect(0, 0, 16,16)
-            avatar.base.drawImage(tilectx, 0, 0);
+            avatar.base.x = 0;
+            avatar.base.y = 0;
+            // tilectx = base.getContext("2d");
+            // tilectx.clearRect(0, 0, 16,16)
+            // avatar.base.drawImage(tilectx, 0, 0);
         }
         else if((tileX==3)&&(tileY<4 || (tileY>4 && tileY<9))){//Pants: [3,0->3 & 5->8]
             avatar.pants = allSprites[tileX][tileY];
-            tilectx = pants.getContext("2d");
-            tilectx.clearRect(0, 0, 16,16)
-            avatar.pants.drawImage(tilectx, 0, 0);
+            avatar.pants.x = 0;
+            avatar.pants.y = 0;
+            // tilectx = pants.getContext("2d");
+            // tilectx.clearRect(0, 0, 16,16)
+            // avatar.pants.drawImage(tilectx, 0, 0);
         }
         else if (((tileX == 3) && (tileY == 4 || tileY == 9))||(tileX==4 &&tileY<10)){//Boots:[3,4/9] || [4,0->9]
             avatar.boots = allSprites[tileX][tileY];
-            tilectx = boots.getContext("2d");
-            tilectx.clearRect(0, 0, 16,16)
-            avatar.boots.drawImage(tilectx, 0, 0);
+            avatar.boots.x = 0;
+            avatar.boots.y = 0;
+            // tilectx = boots.getContext("2d");
+            // tilectx.clearRect(0, 0, 16,16)
+            // avatar.boots.drawImage(tilectx, 0, 0);
         }
         else if ((tileX > 5 && tileX < 18) && (tileY < 10)) {//torso [6->17,0->9]
             avatar.torso = allSprites[tileX][tileY];
-            tilectx = torso.getContext("2d");
-            tilectx.clearRect(0, 0, 16,16)
-            avatar.torso.drawImage(tilectx, 0, 0);
+            avatar.torso.x = 0;
+            avatar.torso.y = 0;
+            // tilectx = torso.getContext("2d");
+            // tilectx.clearRect(0, 0, 16,16)
+            // avatar.torso.drawImage(tilectx, 0, 0);
         }//torso
         else if ((tileX > 18 && tileX < 23) && tileY >7) {
             //hair [19->26,0->2&&4-6] || [19->22,8-10]
             //beard [,3/7] || [,10]
             if (tileY == 10) {
                 avatar.beard = allSprites[tileX][tileY];
-                tilectx = beard.getContext("2d");
-                tilectx.clearRect(0, 0, 16,16);
-                avatar.beard.drawImage(tilectx, 0, 0);
+                avatar.beard.x = 0;
+                avatar.beard.y = 0;
+                // tilectx = beard.getContext("2d");
+                // tilectx.clearRect(0, 0, 16,16);
+                // avatar.beard.drawImage(tilectx, 0, 0);
             } else {
                 avatar.hair = allSprites[tileX][tileY];
-                tilectx = hair.getContext("2d");
-                tilectx.clearRect(0, 0, 16,16);
-                avatar.hair.drawImage(tilectx, 0, 0);
+                avatar.hair.x = 0;
+                avatar.hair.y = 0;
+                // tilectx = hair.getContext("2d");
+                // tilectx.clearRect(0, 0, 16,16);
+                // avatar.hair.drawImage(tilectx, 0, 0);
             }
         }//hair
         else if ((tileX > 18 && tileX < 27) && tileY<8){//(()&&())||((tileX>18 && tileX<23)&&(tileY>7&&tileY<11))) {
             //hair [19->26,0->2&&4-6] || [19->22,8-10]
             if (tileY == 3 || tileY ==7){
                 avatar.beard = allSprites[tileX][tileY];
-                tilectx = beard.getContext("2d");
-                tilectx.clearRect(0, 0, 16,16);
-                avatar.beard.drawImage(tilectx, 0, 0);
+                avatar.beard.x = 0;
+                avatar.beard.y = 0;
+                // tilectx = beard.getContext("2d");
+                // tilectx.clearRect(0, 0, 16,16);
+                // avatar.beard.drawImage(tilectx, 0, 0);
             }else{
                 avatar.hair = allSprites[tileX][tileY];
-                tilectx = hair.getContext("2d");
-                tilectx.clearRect(0, 0, 16,16);
-                avatar.hair.drawImage(tilectx, 0, 0);
+                avatar.hair.x = 0;
+                avatar.hair.y = 0;
+                // tilectx = hair.getContext("2d");
+                // tilectx.clearRect(0, 0, 16,16);
+                // avatar.hair.drawImage(tilectx, 0, 0);
             }
         }//hair
         else if ((tileX > 27 && tileX < 32) && (tileY < 9)){//hat [28->31,0-8]
             avatar.hat = allSprites[tileX][tileY];
-            tilectx = hat.getContext("2d");
-            tilectx.clearRect(0, 0, 16, 16);
-            avatar.hat.drawImage(tilectx, 0, 0);
+            avatar.hat.x = 0;
+            avatar.hat.y = 0;
+            // tilectx = hat.getContext("2d");
+            // tilectx.clearRect(0, 0, 16, 16);
+            // avatar.hat.drawImage(tilectx, 0, 0);
         }
         else if ((tileX > 32 && tileX < 41)&& (tileY < 9)) {//left [33-40,0-8]
             avatar.left = allSprites[tileX][tileY];
-            tilectx = left.getContext("2d");
-            tilectx.clearRect(0, 0, 16, 16);
-            avatar.left.drawImage(tilectx, 0, 0);
+            avatar.left.x = 0;
+            avatar.left.y = 0;
+            // tilectx = left.getContext("2d");
+            // tilectx.clearRect(0, 0, 16, 16);
+            // avatar.left.drawImage(tilectx, 0, 0);
         }//left
         else if ((tileX > 41 && tileX < 52) && (tileY < 10)) {//right [42-51,0-9]
             avatar.right = allSprites[tileX][tileY];
-            tilectx = right.getContext("2d");
-            tilectx.clearRect(0, 0, 16, 16);
-            avatar.right.drawImage(tilectx, 0, 0);
+            avatar.right.x = 0;
+            avatar.right.y = 0;
+            // tilectx = right.getContext("2d");
+            // tilectx.clearRect(0, 0, 16, 16);
+            // avatar.right.drawImage(tilectx, 0, 0);
         }
         else if ((tileX > 51 && tileX < 54) && (tileY < 5)){
             avatar.left = undefined;
-            tilectx = left.getContext("2d");
-            tilectx.clearRect(0, 0, 16, 16);
+            // tilectx = left.getContext("2d");
+            // tilectx.clearRect(0, 0, 16, 16);
             avatar.right = allSprites[tileX][tileY];
-            tilectx = right.getContext("2d");
-            tilectx.clearRect(0, 0, 16, 16);
-            avatar.right.drawImage(tilectx, 0, 0);
+            avatar.right.x = 0;
+            avatar.right.y = 0;
+            // tilectx = right.getContext("2d");
+            // tilectx.clearRect(0, 0, 16, 16);
+            // avatar.right.drawImage(tilectx, 0, 0);
         }
         //Draw new Avatar
         console.log(avatar)
